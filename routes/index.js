21af18c7a7f54dd0b3987fb503e7a9cd845b4884
Diviyansh Bhatnagar <ansh.divu@gmail.com>
2019-06-07 16:11:23 -0400

    Changed `var` to `const`

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,6 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send({ title: 'Express' });
-});
+const router = express.Router();
+router.get('/', (req, res) => res.send({ title: 'Express' }));
 
 module.exports = router;
