692cd56fae6ec7433bdadf06001ff13145e9238c
Heemank <heemankv@gmail.com>
2023-11-22 02:17:09 +0530

    update: python impl

---
+++ Readme.md
@@ -0,0 +1,7 @@
+
+
+
+
+python3 -m pipenv install pyresparser   
+python3 -m spacy download en_core_web_lg
+python -m spacy download en_core_web_sm
---
+++ main.py
@@ -0,0 +1,42 @@
+from pyresparser import ResumeParser
+
+
+def extract_text(file):
+  data = ResumeParser('/content/Heemank_Verma.pdf').get_extracted_data()
+  return data
+
+
+# multiple files are being given as input to the function extract_text and the output is stored in a list
+def extract_text_multiple(files):
+  data = []
+  for file in files:
+    data.append(ResumeParser(file).get_extracted_data())
+  return data
+
+from flask import Flask, jsonify, request 
+
+app = Flask(__name__) 
+
+@app.route('/', methods = ['GET', 'POST']) 
+def home(): 
+	if(request.method == 'GET'): 
+
+		data = "hello world"
+		return jsonify({'data': data}) 
+
+@app.get("/resumeOCR/")
+def resumeOCR():
+    x = extract_text_multiple(["Heemank_Verma.pdf", "Heemank_Verma.pdf"])
+    print(x)
+    return { "data": x}
+
+
+# driver function 
+if __name__ == '__main__': 
+
+	app.run(debug = True) 
+
+
+
+
+
